5e4ac6bb1551df985c10fb836815644daf83e9b6
Marley <andrew.kigen@student.moringaschool.com>
2022-12-03 16:14:57 +0300

    completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+  
+  before_action :authorize 
+
+  private 
+
+  def authorize 
+    @the_user = User.find_by(id: session[:user_id])
+    render json: { errors: ['Unauthorized user']}, status: :unauthorized unless @the_user 
+  end 
+
+  def invalid_record invalid
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity 
+  end
+
 end
